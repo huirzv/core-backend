2b3e591a62fb3518a9cce451571c5093ba5bca21
huirzv <114877689+huirzv@users.noreply.github.com>
2025-06-06 00:52:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 const app = express();
-const contactRoute = require("./routes/contact");
-const gamesRoute = require("./routes/games");
-const groupsRoute = require("./routes/groups");
-const statsRoute = require("./routes/stats");
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/contact", contactRoute);
-app.use("/api/games", gamesRoute);
-app.use("/api/groups", groupsRoute);
-app.use("/api/stats", statsRoute);
+app.use('/routes/contact', require('./routes/contact'));
+app.use('/routes/games', require('./routes/games'));
+app.use('/routes/groups', require('./routes/groups'));
 
+app.get('/', (req, res) => res.send('Backend is running.'));
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server running on port " + PORT));
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
